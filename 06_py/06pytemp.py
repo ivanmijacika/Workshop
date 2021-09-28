61076662a3036e36e41404a4caa20e7a513ef3b4
ivanmijacika <imijacika20@stuy.edu>
2021-09-28 08:34:39 -0400

    file with temporary name ddedd

---
+++ 06_py/06pytemp.py
@@ -0,0 +1,14 @@
+import random
+import csv
+
+def readMethod():
+    
+    
+    
+#with open('FILENAME', newline='') as csvfile:
+#    READERNAME = csv.reader(csvfile, delimeter='', quotechar='|')  <== what is quotechar, delimeter
+#    for row in spamreader:
+#        print(', '.join(row))   <== return formatting
+
+#use read, realine, or readlines
+#text reader vs csv
