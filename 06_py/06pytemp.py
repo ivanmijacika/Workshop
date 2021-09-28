2147e43e2b45cbce82b7900a83f60c6164a9355a
ivanmijacika <imijacika20@stuy.edu>
2021-09-28 08:40:21 -0400

    chagned some names and mixed commentted text into code

--- 06_py/06pytemp.py
+++ 06_py/06pytemp.py
@@ -2,13 +2,11 @@ import random
 import csv
 
 def readMethod():
-    
-    
-    
-#with open('FILENAME', newline='') as csvfile:
-#    READERNAME = csv.reader(csvfile, delimeter='', quotechar='|')  <== what is quotechar, delimeter
-#    for row in spamreader:
-#        print(', '.join(row))   <== return formatting
+    with open('FILENAME', newline='') as csvfile:
+        jobReader = csv.reader(csvfile, delimeter='', quotechar='|') # <== what is quotechar, delimeter
+        for row in jobReader:
+            print(', '.join(row))  # <== return formatting
 
 #use read, realine, or readlines
 #text reader vs csv
+#.append if adding to a list
